09f47334c1aaaf325d2b3f8445d13fea6d312cf0
StasyaShu <stanislava.semenova@gmail.com>
2023-01-24 18:18:07 +0300

    Верстка и стили шапки

--- source/js/main.js
+++ source/js/main.js
@@ -1,8 +1,10 @@
 import * as flsFunctions from './modules/functions.js';
 // import {initSwiper} from './slider/initSwiper.js';
+import {initCustomSelect} from './modules/init-custom-select.js';
 
 
 flsFunctions.isWebp();
+initCustomSelect();
   // initSwiper();
 
 
---
+++ source/js/modules/init-custom-select.js
@@ -0,0 +1,8 @@
+import {CustomSelect} from './custom-select.js';
+
+const initCustomSelect = () => {
+  const select = new CustomSelect();
+  select.init();
+};
+
+export {initCustomSelect};
