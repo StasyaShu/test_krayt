4cada1c3788d1c1cc839dcd78fcbeb79332a9497
StasyaShu <stanislava.semenova@gmail.com>
2023-01-27 23:25:51 +0300

    убирает ошибку деплоя

--- source/js/main.js
+++ source/js/main.js
@@ -1,11 +1,9 @@
-import * as flsFunctions from './utils/functions.js';
 import { initSwiper } from './slider/initSwiper.js';
 import { initCustomSelect } from './modules/init-custom-select.js';
 import { initTimer } from './modules/init-timer.js';
 import { manageMobMenu } from './modules/init-mobile-menu.js';
 
 window.addEventListener('DOMContentLoaded', () => {
-  flsFunctions.isWebp();
   initCustomSelect();
   initSwiper();
   initTimer();
