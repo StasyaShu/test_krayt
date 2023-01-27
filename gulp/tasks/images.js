d36640f0d5794cca14b818c0cb4101541bb570a0
StasyaShu <stanislava.semenova@gmail.com>
2023-01-27 20:04:53 +0300

    пересобирает билд

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -18,7 +18,7 @@ export const images = () => {
             progressive: true,
             svgoPlugins: [{removeViewBox: false}],
             interlaced: true,
-            optimizationLevel: 3 //0 to 7
+            optimizationLevel: 5 //0 to 7
         }))
         .pipe(app.gulp.dest(app.path.build.images))
         .pipe(app.gulp.src(app.path.src.svg))
