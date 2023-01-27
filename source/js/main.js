7f531bba927dd191b96922b4086121b3d2cd108f
StasyaShu <stanislava.semenova@gmail.com>
2023-01-27 21:03:31 +0300

    тест в разных браузерах

--- source/js/main.js
+++ source/js/main.js
@@ -1,8 +1,8 @@
 import * as flsFunctions from './utils/functions.js';
-import {initSwiper} from './slider/initSwiper.js';
-import {initCustomSelect} from './modules/init-custom-select.js';
-import {initTimer} from './modules/init-timer.js';
-import {manageMobMenu} from './modules/init-mobile-menu.js';
+import { initSwiper } from './slider/initSwiper.js';
+import { initCustomSelect } from './modules/init-custom-select.js';
+import { initTimer } from './modules/init-timer.js';
+import { manageMobMenu } from './modules/init-mobile-menu.js';
 
 window.addEventListener('DOMContentLoaded', () => {
   flsFunctions.isWebp();
